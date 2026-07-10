abb944e65ac767c7ac182d8f4eaef7eede155686
KacperGryczon <kacper.gryczon@gmail.com>
2026-07-10 23:07:59 +0200

    test

---
+++ script.js
@@ -0,0 +1,29 @@
+const navEl = document.querySelector("nav");
+const hamburgerEl = document.querySelector(".hamburger");
+
+hamburgerEl.addEventListener("click", () => {
+  navEl.classList.toggle("nav-open");
+  hamburgerEl.classList.toggle("hamburger-open");
+});
+
+const headerEl = document.querySelector("header");
+const scrollEl = document.querySelector(".scroll-animation");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    headerEl.classList.add("header-scrolled");
+    scrollEl.classList.add("disabled");
+  } else if (window.scrollY <= 50) {
+    headerEl.classList.remove("header-scrolled");
+  }
+});
+
+const hotelCardEl = document
+  .querySelector(".hotel-cards")
+  .querySelectorAll(".card");
+
+const hotelImgEl = hotelCardEl.document
+  .querySelector(".img-container")
+  .querySelector("img");
+
+console.log(hotelImgEl);
